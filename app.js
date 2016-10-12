8c01a86e51223e409838ff9603d0e4a8da78884c
James Cool <jeffreysbrother@gmail.com>
2016-10-11 23:23:28 -0700

    add form validation by adding the ngMessages angular module

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // MODULE
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
 // CONTROLLERS
 myApp.controller('mainController', function($scope, $log) {
+  $log.log("hello");
+  $log.info("This is INFO");
+  $log.warn("Warning!!!");
+  $log.debug("debug info");
+  $log.error("That's a goddamn error, bro");
   console.log($scope);
   console.log($log);
 });
